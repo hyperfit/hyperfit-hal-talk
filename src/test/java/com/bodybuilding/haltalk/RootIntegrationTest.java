f8674050acca28428fb2d1e1c7ba7de8c67abf14
Chris DaMour <drdamour@gmail.com>
2015-06-03 13:40:48 -0600

    got something going

--- src/test/java/com/bodybuilding/haltalk/RootIntegrationTest.java
+++ src/test/java/com/bodybuilding/haltalk/RootIntegrationTest.java
@@ -2,7 +2,10 @@ package com.bodybuilding.haltalk;
 
 import org.hyperfit.HyperfitProcessor;
 import org.hyperfit.content.hal.json.HalJsonContentTypeHandler;
+import org.hyperfit.net.okhttp2.OkHttp2HyperClient;
+import org.hyperfit.resource.HyperResource;
 import org.junit.Test;
+import utils.Helpers;
 
 public class RootIntegrationTest {
 
@@ -10,8 +13,10 @@ public class RootIntegrationTest {
     public void testRootRetrieval() {
         HyperfitProcessor processor = new HyperfitProcessor.Builder()
             .addContentTypeHandler(new HalJsonContentTypeHandler())
+            .hyperClient(new OkHttp2HyperClient(Helpers.allTrustingOkHttpClient()))
             .build();
 
+        HyperResource resource = processor.processRequest(HyperResource.class, ContractConstants.rootURL);
 
     }
 
